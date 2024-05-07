668cdfbc1c5e80f6726454d0872213a5e332a417
sina.gilassi <sina.gilassi@gmail.com>
2024-05-07 12:47:12 -0400

    add class for comp

--- pyThermoDB/docs/transdata.py
+++ pyThermoDB/docs/transdata.py
@@ -1,5 +1,6 @@
 # import packages/modules
-
+# external
+import pandas as pd
 
 class TransData:
     '''
@@ -18,5 +19,15 @@ class TransData:
 
         for x,y,z in zip(self.api_data['header'], self.api_data['records'], self.api_data['unit']):
             data_trans[str(x)] = {"value": y, "unit": z}
+            
+        # data table
+        data_trans['data'] = self.api_data
 
         return data_trans
+    
+    def view(self):
+        '''
+        display data in a table (pandas dataframe)
+        '''
+        df = pd.DataFrame(self.api_data)
+        print(df)
