7e0979ca188a7d925753a2985a79c11486d3f2bf
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-09-12 21:05:43 -0400

    add CO2 Hydrogenation

--- tests/test4.py
+++ tests/test4.py
@@ -13,11 +13,11 @@ print(ptdb.__version__)
 # CREATE CUSTOM REFERENCE
 # ===============================
 # files
-yml_file = 'tests\\ref1.yml'
+yml_file = 'tests\\CO2 Hydrogenation.yml'
 yml_path = os.path.join(os.getcwd(), yml_file)
 
-csv_file_1 = 'tests\\Table A.II The Molar Heat Capacities of Gases in the Ideal Gas (Zero-Pressure) State.csv'
-csv_file_2 = 'tests\\Table A.IV Enthalpies and Gibbs Energies of Formation.csv'
+csv_file_1 = 'tests\\The Molar Heat Capacities of Gases in the Ideal Gas (Zero-Pressure) State.csv'
+csv_file_2 = 'tests\\General Data.csv'
 csv_path_1 = os.path.join(os.getcwd(), csv_file_1)
 csv_path_2 = os.path.join(os.getcwd(), csv_file_2)
 
--- tests/thermodb_CO2_load.py
+++ tests/thermodb_CO2_load.py
@@ -21,5 +21,29 @@ pp(CO2_thermodb.check_properties())
 
 CO2_general = CO2_thermodb.check_property('general')
 pp(type(CO2_general))
-pp(CO2_general.get_property(
-    'dHf_IG')['value'])
+# heat of formation at 298.15K
+CO2_dHf_IG = float(CO2_general.get_property('dHf_IG')['value'])
+CO2_dHf_IG_unit = CO2_general.get_property('dHf_IG')['unit']
+CO2_dHf_IG_J__mol = CO2_dHf_IG*1000
+pp(f'CO2_dHf_IG = {CO2_dHf_IG} {CO2_dHf_IG_unit}, {CO2_dHf_IG_J__mol} J/mol')
+# Gibbs free energy of formation at 298.15 K
+CO2_dGf_IG = float(CO2_general.get_property('dGf_IG')['value'])
+pp(f'CO2_dGf_IG = {CO2_dGf_IG}')
+
+# equation
+CO2_Cp_eq = CO2_thermodb.check_function('heat-capacity')
+# args
+pp(CO2_Cp_eq.args)
+# return
+pp(CO2_Cp_eq.returns)
+# cal
+CO2_Cp_cal = CO2_Cp_eq.cal(T=298.15)
+pp(f'CO2_Cp_cal = {CO2_Cp_cal}')
+
+# calculate dH at 320
+CO2_Cp_integral = CO2_Cp_eq.cal_integral(T1=298.15, T2=320)
+pp(f'CO2_Cp_integral = {CO2_Cp_integral}')
+
+# enthalpy at 320 K
+CO2_Cp_enthalpy = (CO2_dHf_IG_J__mol + CO2_Cp_integral)/1000
+pp(f'CO2_Cp_enthalpy = {CO2_Cp_enthalpy}')
