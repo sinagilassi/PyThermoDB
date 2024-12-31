2c917d0ce718d01b2a3576e07834f9e7c5597bf2
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2024-12-30 21:47:04 -0500

    new version 1.7.3

--- pyThermoDB/config/constants.py
+++ pyThermoDB/config/constants.py
@@ -1,4 +1,6 @@
 # pyThermoDB version
-__version__ = "1.7.2"
+__version__ = "1.7.3"
 # author
 __author__ = "Sina Gilassi"
+# description
+__description__ = "PyThermoDB is a lightweight and user-friendly Python package designed to provide quick access to essential thermodynamic data."
