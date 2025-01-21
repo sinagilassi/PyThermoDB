1a290aff4c6d05734fbc782f9c41a9b26cb3582b
sina.gilassi <sina.gilassi@gmail.com>
2025-01-21 01:06:53 -0500

    add

--- app/home.py
+++ app/home.py
@@ -1,7 +1,7 @@
-import streamlit as st    
+import streamlit as st
 
 # title
-st.title('Home')
+st.title('Welcome to PyThermoDB')
 
 # description
 st.write('This app is a simple web interface for the PyThermoDB Python package. It allows users to search for thermodynamic data for chemical species and reactions.')
@@ -12,7 +12,7 @@ import pyThermoDB as ptdb
 # check version
 print(ptdb.__version__)
 '''
-st.code(code,language='python', line_numbers=False)
+st.code(code, language='python', line_numbers=False)
 
 # section title
 st.header('Features')
@@ -26,9 +26,11 @@ st.markdown('''
 
 
 # content
-st.write('Check out examples of how to use the PyThermoDB package in the [documentation](https://pythermodb.readthedocs.io/en/latest/).')
+st.write(
+    'Check out examples of how to use the PyThermoDB package in the [documentation](https://pythermodb.readthedocs.io/en/latest/).')
 
 st.write('You can also have a look at `tests folder` in the github repository to see how the package is used.')
 
 # ask question
-st.write('If you have any questions or would like to report a bug, please open an issue in the [github repository](https://github.com/sinagilassi/PyThermoDB/issues)')
+st.write(
+    'If you have any questions or would like to report a bug, please open an issue in the [github repository](https://github.com/sinagilassi/PyThermoDB/issues)')
--- app/index.py
+++ app/index.py
@@ -1,13 +1,14 @@
-import streamlit as st 
+import streamlit as st
 
 # page config
-st.set_page_config(page_title='PyThermoDB App', page_icon='🧊', layout='wide', initial_sidebar_state='expanded')
+st.set_page_config(page_title='PyThermoDB App', page_icon='🧊',
+                   layout='wide', initial_sidebar_state='expanded')
 
 # pages
 pages = [
-    st.Page('home.py',title='Home',icon='🧊'),
-    st.Page('search.py',title='Search',icon='🔍'),
-    st.Page('about.py',title='About',icon='📚'),
+    st.Page('home.py', title='Home', icon='🧊'),
+    st.Page('search.py', title='Search', icon='🔍'),
+    st.Page('about.py', title='About', icon='📚'),
 ]
 
 pg = st.navigation(pages)
