c833dc16cf84f9dd9adb8711d95bc28fde320c39
sina.gilassi <sina.gilassi@gmail.com>
2025-02-16 16:41:12 -0500

    add description in search databook

---
+++ tests/check-component.py
@@ -0,0 +1,54 @@
+# import packages/modules
+import pyThermoDB as ptdb
+from pprint import pprint as pp
+from rich import print
+
+
+# dir
+# print(dir(pt))
+# get versions
+# print(pt.get_version())
+print(ptdb.__version__)
+print(ptdb.__author__)
+print(ptdb.__description__)
+
+# ===============================
+# databook reference initialization
+# ===============================
+tdb = ptdb.init()
+
+# ====================================
+# SEARCH IN THE DATABOOK
+# ====================================
+# search terms
+key1 = 'Carbon dioxide'
+key2 = 'CO2'
+search_terms = [key1, key2]
+# search_terms = [key1]
+search_res = tdb.search_databook(
+    search_terms, res_format='json', search_mode='exact')
+print(search_res)
+
+# search terms
+# looking through Name
+key1 = 'Carbon dioxide'
+# search terms
+search_terms = [key1]
+# # column names
+# column_names = ['Name']
+# start search
+search_res = tdb.search_databook(
+    search_terms, res_format='dict', search_mode='exact')
+print(search_res)
+
+# search terms
+# looking through Formula
+key1 = 'CO2'
+# search terms
+search_terms = [key1]
+# column names
+column_names = ['Formula']
+# start search
+search_res = tdb.search_databook(
+    search_terms, res_format='json', search_mode='exact', column_names=column_names)
+print(search_res)
