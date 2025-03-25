9b499de6ba6e3be793f6a2d5effab18d9910bd94
sina.gilassi <sina.gilassi@gmail.com>
2025-03-25 00:20:27 -0400

    fix bugs

--- pyThermoDB/__init__.py
+++ pyThermoDB/__init__.py
@@ -1,10 +1,8 @@
 from .app import (
     desc, get_version, init, ref, __version__, build_thermodb, load_thermodb,
     __author__, __description__,
-    TableData, TableEquation, TableMatrixData, TableMatrixEquation
 )
 
 __all__ = ['desc', 'get_version', 'init',
            'ref', '__version__', 'build_thermodb', 'load_thermodb',
-           '__author__', '__description__', 'TableData', 'TableEquation',
-           'TableMatrixData', 'TableMatrixEquation']
+           '__author__', '__description__', ]
