ef24a63b90df008cf5cbbdc1016466a8b679cee4
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-06-07 20:01:24 -0400

    upgrade v1.10.15

--- pyThermoDB/config/setting.py
+++ pyThermoDB/config/setting.py
@@ -3,4 +3,4 @@
 
 
 # api url
-API_URL = "https://script.google.com/macros/s/AKfycbzA78GeYUGfwNq_9aeQyNeP3dG82T5NehqW_SJzWBDFXHQiD7DCQuZlGFmZ9kVuZvdX/exec"
+API_URL = "https://script.google.com/macros/s/"
