6bdd4b518ac9d423df8083593cdd4295db7be447
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-09-01 22:54:29 -0400

    add util

--- pyThermoDB/utils/__init__.py
+++ pyThermoDB/utils/__init__.py
@@ -2,11 +2,14 @@ from .logger import log2Col
 from .utility import isNumber, uppercaseStringList
 from .result_generator import format_eq_data
 from .convertor import Convertor
+from .component_utils import set_component_id, set_component_query
 
 __all__ = [
     "log2Col",
     "isNumber",
     "uppercaseStringList",
     "format_eq_data",
-    "Convertor"
+    "Convertor",
+    "set_component_id",
+    "set_component_query",
 ]
