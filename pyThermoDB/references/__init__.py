91e56e9ede66fc5d51f05a366c107ed7cf3c2da1
sina.gilassi <sina.gilassi@gmail.com>
2025-09-06 01:17:02 -0400

    add import

--- pyThermoDB/references/__init__.py
+++ pyThermoDB/references/__init__.py
@@ -4,6 +4,7 @@ from .databook import ThermoDatabook
 from .reference import ThermoReference
 from .checker import ReferenceChecker
 from .main import check_custom_reference, load_reference_from_str
+from .reference_mapper import map_component_reference
 
 __all__ = [
     "ReferenceConfig",
@@ -13,5 +14,6 @@ __all__ = [
     "ThermoReference",
     "ReferenceChecker",
     "check_custom_reference",
-    "load_reference_from_str"
+    "load_reference_from_str",
+    "map_component_reference",
 ]
