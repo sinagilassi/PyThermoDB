769b4c29ef8f0e5df094fc5a213f57443db2e207
sina.gilassi <sina.gilassi@gmail.com>
2025-09-11 22:12:12 -0400

    add extract function

--- pyThermoDB/references/__init__.py
+++ pyThermoDB/references/__init__.py
@@ -3,7 +3,7 @@ from .content import ReferenceContent, load_custom_reference
 from .databook import ThermoDatabook
 from .reference import ThermoReference
 from .checker import ReferenceChecker
-from .main import check_custom_reference, load_reference_from_str
+from .main import check_custom_reference, load_reference_from_str, extract_reference_from_str
 from .reference_mapper import component_reference_mapper
 
 __all__ = [
@@ -16,4 +16,5 @@ __all__ = [
     "check_custom_reference",
     "load_reference_from_str",
     "component_reference_mapper",
+    "extract_reference_from_str",
 ]
