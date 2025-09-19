28c63bbac3b17ef615e05defbafded44594b0d61
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-09-19 16:51:41 -0400

    test done

---
+++ examples/matrix-1/build matrix thermodb.py
@@ -0,0 +1,161 @@
+# import packages/modules
+import pyThermoDB as ptdb
+from pyThermoDB.core import TableMatrixData
+import os
+from rich import print
+from typing import Dict, Any
+
+# version
+print(ptdb.__version__)
+
+# ====================================
+# CUSTOM REFERENCES
+# ====================================
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+print(f"Parent directory: {parent_dir}")
+
+# files
+yml_file = 'nrtl-3.yml'
+yml_path = os.path.join(parent_dir, yml_file)
+# csv files (data/equation tables)
+# ! new format
+csv_file_1 = 'Non-randomness parameters of the NRTL equation-3.csv'
+csv_path_1 = os.path.join(parent_dir, csv_file_1)
+
+# custom ref
+ref: Dict[str, Any] = {
+    'reference': [yml_path],
+    'tables': [csv_path_1]
+}
+
+# ====================================
+# INITIALIZATION OWN THERMO DB
+# ====================================
+thermo_db = ptdb.init(custom_reference=ref)
+
+# ====================================
+# GET DATABOOK LIST
+# ====================================
+db_list = thermo_db.list_databooks()
+print(db_list)
+
+# ====================================
+# SELECT A DATABOOK
+# ====================================
+# table list
+tb_list = thermo_db.list_tables('NRTL')
+print(tb_list)
+
+# ====================================
+# CHECK COMPONENT AVAILABILITY IN A TABLE
+# ====================================
+# check component availability in the databook and table
+comp1 = "methanol"
+# COMP1_check_availability = thermo_db.check_component(
+#     comp1, 'NRTL', "Non-randomness parameters of the NRTL equation")
+
+comp2 = "ethanol"
+# COMP1_check_availability = thermo_db.check_component(
+#     comp2, 'NRTL', "Non-randomness parameters of the NRTL equation")
+
+# comp3
+comp3 = 'benzene'
+# query
+# query = f"Name.str.lower() == '{comp1.lower()}' & State == 'g'"
+# COMP1_check_availability = thermo_db.check_component(
+#     comp1, 3, 2, query, query=True)
+
+components = [comp1, comp2, comp3]
+
+# ====================================
+# LOAD MATRIX DATA
+# ====================================
+tb_data_df = thermo_db.table_data(
+    'NRTL',
+    1
+)
+print(type(tb_data_df))
+print(tb_data_df)
+
+# ====================================
+# BUILD MATRIX DATA
+# ====================================
+# NOTE: build a matrix data
+nrtl_alpha = thermo_db.build_thermo_property(
+    [comp1, comp2], 'NRTL', "Non-randomness parameters of the NRTL equation-3")
+# check type
+if not isinstance(nrtl_alpha, TableMatrixData):
+    raise TypeError("nrtl_alpha is not an instance of TableMatrixData")
+
+# matrix table
+print(nrtl_alpha.matrix_table)
+# matrix table
+print(nrtl_alpha.get_matrix_table(mode='selected'))
+# symbol
+print(nrtl_alpha.matrix_symbol)
+
+print(nrtl_alpha.matrix_data_structure())
+
+print(nrtl_alpha.get_property('Alpha_i_1', comp1))
+# print(nrtl_alpha.get_property(4, comp1))
+# by symbol
+# print(float(Alpha_i_j['value']))
+
+print(nrtl_alpha.get_matrix_property(
+    "Alpha_i_j",
+    [comp1, comp2], symbol_format='alphabetic', message="NRTL Alpha value")
+)
+
+print(nrtl_alpha.get_matrix_property(
+    "Beta_i_j",
+    [comp1, comp2], symbol_format='alphabetic', message="NRTL Alpha value")
+)
+
+# property name using ij method
+prop_name = f"Alpha_{comp1}_{comp3}"
+print(prop_name)
+print(nrtl_alpha.ij(prop_name))
+print(nrtl_alpha.ij(prop_name).get('value'))
+
+
+# matrix data
+res_1 = nrtl_alpha.mat("Alpha", [comp2, comp1, comp3])
+print(res_1)
+
+# metric
+print(nrtl_alpha.ijs(f"Alpha | {comp1} | {comp2}"))
+
+# components
+# looping through the matrix data
+for comp1 in components:
+    for comp2 in components:
+        prop_name = f"Delta_{comp1}_{comp2}"
+        # get property value
+        prop_value = nrtl_alpha.ij(prop_name).get('value')
+        # log
+        print(f"Property: {prop_name} = {prop_value}")
+
+
+# ====================================
+# BUILD THERMODB
+# ====================================
+# thermodb name
+thermodb_name = "thermodb_nrtl_2"
+
+# build a thermodb
+thermo_db = ptdb.build_thermodb()
+print(type(thermo_db))
+
+# NOTE: add TableMatrixData
+thermo_db.add_data('nrtl', nrtl_alpha)
+
+# save
+thermo_db.save(
+    f'{thermodb_name}', file_path=parent_dir)
+
+# ====================================
+# CHECK THERMODB
+# ====================================
+# check all properties and functions registered
+print(thermo_db.check_properties())
+print(thermo_db.check_functions())
