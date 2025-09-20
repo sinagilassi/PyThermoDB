385fa474e317f69d3c906c21953e8eced0f04df7
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-09-19 23:26:37 -0400

    add check mixture component availability

--- examples/configs/exp-build-thermodb-1.py
+++ examples/configs/exp-build-thermodb-1.py
@@ -160,7 +160,7 @@ REFERENCES:
           VALUES:
             - [1,methanol|ethanol,methanol,CH3OH,0,0.300492719,0,1.564200272,0,35.05450323,0,4.481683583]
             - [2,methanol|ethanol,ethanol,C2H5OH,0.380229054,0,-20.63243601,0,0.059982839,0,4.481683583,0]
-            - [1,methane|ethanol,methanol,CH3OH,0,0.300492719,0,1.564200272,0,35.05450323,0,4.481683583]
+            - [1,methane|ethanol,methane,CH4,0,0.300492719,0,1.564200272,0,35.05450323,0,4.481683583]
             - [2,methane|ethanol,ethanol,C2H5OH,0.380229054,0,-20.63243601,0,0.059982839,0,4.481683583,0]
 """
 # NOTE: parent directory of this file
