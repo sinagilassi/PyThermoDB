e957017d82fe85cdf0d96500d9ac354968143738
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-09-29 13:57:58 -0400

    tets done for components

--- examples/configs/doc3 - matrix table.py
+++ examples/configs/doc3 - matrix table.py
@@ -1,49 +1,28 @@
-# import packages/modules
-from typing import Dict, List, Any
+# import libs
 import os
-from rich import print
-import pyThermoDB as ptdb
 from pyThermoDB import (
-    build_components_thermodb,
-    check_and_build_components_thermodb,
-    build_components_thermodb_from_reference
+    build_component_thermodb_from_reference,
+    ComponentThermoDB,
+    build_components_thermodb_from_reference,
+    MixtureThermoDB
 )
-from pyThermoDB.core import TableMatrixData
-from pyThermoDB.references import ReferenceConfig
 from pythermodb_settings.models import Component
+from rich import print
 
-# versions
-print(ptdb.__version__)
-
-# ====================================
-# CUSTOM REFERENCES
-# ====================================
-# parent directory
-parent_dir = os.path.dirname(os.path.abspath(__file__))
-
-# files
-# SECTION:
-yml_file = 'str-ref-2.yml'
-yml_path = os.path.join(parent_dir, yml_file)
-
-# SECTION:
-md_file = 'str-ref-1.md'
-md_path = os.path.join(parent_dir, md_file)
-
-# SECTION: file contents
-file_contents = """
+# SECTION: reference content
+REFERENCE_CONTENT = """
 REFERENCES:
     CUSTOM-REF-1:
       DATABOOK-ID: 1
       TABLES:
-        Ideal-Gas-Molar-Heat-Capacity:
+        ideal-gas-heat-capacity:
           TABLE-ID: 1
           DESCRIPTION:
             This table provides the heat capacity at constant pressure of ideal gas (Cp_IG) in J/mol.K as a function of temperature (T) in K.
           EQUATIONS:
             EQ-1:
               BODY:
-                - res['heat capacity of ideal gas | Cp_IG | J/mol.K'] = (parms['a0 | a0 | 1'] + parms['a1 | a1 | 1E3']*args['temperature | T | K'] + parms['a2 | a2 | 1E5']*(args['temperature | T | K']**2) + parms['a3 | a3 | 1E8']*(args['temperature | T | K']**3) + parms['a4 | a4 | 1E11']*(args['temperature | T | K']**4))*parms['Universal-Gas-Constant | R | J/mol.K']
+                - res['ideal-gas-heat-capacity | Cp_IG | J/mol.K'] = (parms['a0 | a0 | 1'] + parms['a1 | a1 | 1E3']*args['temperature | T | K'] + parms['a2 | a2 | 1E5']*(args['temperature | T | K']**2) + parms['a3 | a3 | 1E8']*(args['temperature | T | K']**3) + parms['a4 | a4 | 1E11']*(args['temperature | T | K']**4))*parms['Universal-Gas-Constant | R | J/mol.K']
               BODY-INTEGRAL:
                   - A1 = parms['a0 | a0 | 1']*args['temperature | T1 | K']
                   - B1 = (parms['a1 | a1 | 1E3']/2)*(args['temperature | T1 | K']**2)
@@ -67,7 +46,7 @@ REFERENCES:
             SYMBOL: [None,None,None,None,a0,a1,a2,a3,a4,R,Cp_IG]
             UNIT: [None,None,None,None,1,1E3,1E5,1E8,1E11,1,J/mol.K]
           VALUES:
-            - [1,'carbon dioxide','CO2','g',3.259,1.356,1.502,-2.374,1.056,8.314,1]
+            - [1,'carbon dioxide','CO2','l',3.259,1.356,1.502,-2.374,1.056,8.314,1]
             - [2,'carbon monoxide','CO','g',3.912,-3.913,1.182,-1.302,0.515,8.314,1]
             - [3,'hydrogen','H2','g',2.883,3.681,-0.772,0.692,-0.213,8.314,1]
             - [4,'methanol','CH3OH','g',4.714,-6.986,4.211,-4.443,1.535,8.314,1]
@@ -83,7 +62,7 @@ REFERENCES:
             - [14,'benzene','C6H6','l',3.551,-6.184,14.365,-19.807,8.234,8.314,1]
             - [15,'nitrogen','N2','g',3.539,-0.261,0.007,0.157,-0.099,8.314,1]
             - [16,'ethane','C2H6','g',4.178,-4.427,5.660,-6.651,2.487,8.314,1]
-        General-Data:
+        general-data:
           TABLE-ID: 2
           DESCRIPTION:
             This table provides the general data of different chemical species participating in the CO2 hydrogenation reaction and includes molecular weight (MW) in g/mol, critical temperature (Tc) in K, critical pressure (Pc) in MPa, and critical molar volume (Vc) in m3/kmol. The table also includes the critical compressibility factor (Zc), acentric factor (AcFa), enthalpy of formation (EnFo) in kJ/mol, and Gibbs energy of formation (GiEnFo) in kJ/mol. The chemical state of the species is also provided in the table and hence the enthalpy of formation and Gibbs energy of formation are provided for the ideal gas and liquid state are designated as EnFo_IG, GiEnFo_IG, EnFo_LIQ, and GiEnFo_LIQ, respectively.
@@ -110,7 +89,7 @@ REFERENCES:
             - [14,'benzene','C6H6','l',78.112,562.05,4.895,0.256,0.268,0.2103,-49.10,124.5]
             - [15,'nitrogen','N2','g',28.013,126.2,3.4,0.08921,0.289,0.0377,0,0]
             - [16,'ethane','C2H6','g',30.069,305.32,4.872,0.1455,0.279,0.0995,-83.8,-31.9]
-        Vapor-Pressure:
+        vapor-pressure:
           TABLE-ID: 3
           DESCRIPTION:
             This table provides the vapor pressure (P) in Pa as a function of temperature (T) in K.
@@ -134,7 +113,7 @@ REFERENCES:
             SYMBOL: [None,None,None,None,C1,C2,C3,C4,C5,Tmin,P(Tmin),Tmax,P(Tmax),VaPr]
             UNIT: [None,None,None,None,1,1,1,1,1,K,Pa,K,Pa,Pa]
           VALUES:
-            - [1,'carbon dioxide','CO2','l',140.54,-4735,-21.268,4.09E-02,1,216.58,5.19E+05,304.21,7.39E+06,1]
+            - [1,'carbon dioxide','CO2','g',140.54,-4735,-21.268,4.09E-02,1,216.58,5.19E+05,304.21,7.39E+06,1]
             - [2,'carbon monoxide','CO','g',45.698,-1076.6,-4.8814,7.57E-05,2,68.15,1.54E+04,132.92,3.49E+06,1]
             - [3,'hydrogen','H2','g',12.69,-94.9,1.1125,3.29E-04,2,13.95,7.21E+03,33.19,1.32E+06,1]
             - [4,'methanol','CH3OH','g',82.718,-6904.5,-8.8622,7.47E-06,2,175.47,1.11E-01,512.5,8.15E+06,1]
@@ -155,7 +134,25 @@ REFERENCES:
           DESCRIPTION:
             This table provides the NRTL non-randomness parameters for the NRTL equation.
           MATRIX-SYMBOL:
-            - a-constant: a
+            - a
+            - b
+            - c
+            - alpha
+          STRUCTURE:
+            COLUMNS: [No.,Name,Formula,a_i_1,a_i_2,b_i_1,b_i_2,c_i_1,c_i_2,alpha_i_1,alpha_i_2]
+            SYMBOL: [None,None,None,a_i_1,a_i_2,b_i_1,b_i_2,c_i_1,c_i_2,alpha_i_1,alpha_i_2]
+            UNIT: [None,None,None,1,1,1,1,1,1,1,1]
+          VALUES:
+            - [None,None,None,methanol,ethanol,methanol,ethanol,methanol,ethanol,methanol,ethanol]
+            - [None,None,None,CH3OH,C2H5OH,CH3OH,C2H5OH,CH3OH,C2H5OH,CH3OH,C2H5OH]
+            - [1,methanol,CH3OH,0,0.300492719,0,1.564200272,0,35.05450323,0,4.481683583]
+            - [2,ethanol,C2H5OH,0.380229054,0,-20.63243601,0,0.059982839,0,4.481683583,0]
+        NRTL Non-randomness parameters-2:
+          TABLE-ID: 5
+          DESCRIPTION:
+            This table provides the NRTL non-randomness parameters for the NRTL equation.
+          MATRIX-SYMBOL:
+            - a constant: a
             - b
             - c
             - alpha
@@ -164,75 +161,21 @@ REFERENCES:
             SYMBOL: [None,None,None,None,None,a_i_1,a_i_2,b_i_1,b_i_2,c_i_1,c_i_2,alpha_i_1,alpha_i_2]
             UNIT: [None,None,None,None,None,1,1,1,1,1,1,1,1]
           VALUES:
-            - [1,methanol|ethanol,methanol,CH3OH,l,0,1,1,1.564200272,0,35.05450323,0,4.481683583]
-            - [2,methanol|ethanol,ethanol,C2H5OH,g,2,3,-20.63243601,0,0.059982839,0,4.481683583,0]
-            - [1,methanol|methane,methanol,CH3OH,l,1,0.300492719,0,1.564200272,0,35.05450323,0,4.481683583]
-            - [2,methanol|methane,methane,CH4,g,0.380229054,0,-20.63243601,0,0.059982839,0,4.481683583,0]
+            - [1,methanol|ethanol,methanol,CH3OH,l,0,0.300492719,0,1.564200272,0,35.05450323,0,4.481683583]
+            - [2,methanol|ethanol,ethanol,C2H5OH,l,0.380229054,0,-20.63243601,0,0.059982839,0,4.481683583,0]
+            - [1,methane|ethanol,methane,CH4,g,0,0.300492719,0,1.564200272,0,35.05450323,0,4.481683583]
+            - [2,methane|ethanol,ethanol,C2H5OH,l,0.380229054,0,-20.63243601,0,0.059982839,0,4.481683583,0]
 """
 
-# NOTE: custom ref
-custom_reference: Dict[str, Any] = {'reference': [file_contents]}
-# md ref
-# custom_reference = {'reference': [md_path]}
-# yml ref
-# custom_reference: Dict[str, Any] = {'reference': [yml_path]}#
+# NOTE: current file path
+parent_path = os.path.dirname(os.path.abspath(__file__))
+print(parent_path)
 
-# ====================================
-# INITIALIZATION OWN THERMO DB
-# ====================================
-thermo_db = ptdb.init(custom_reference=custom_reference)
+# SECTION: check component availability
+component_name = 'carbon dioxide'
+component_formula = 'CO2'
+component_state = 'g'
 
-# ====================================
-# GET DATABOOK LIST
-# ====================================
-db_list = thermo_db.list_databooks()
-print(db_list)
-
-# ====================================
-# SELECT A DATABOOK
-# ====================================
-# NOTE: set databook
-databook_ = 'CUSTOM-REF-1'
-
-# table list
-tb_list = thermo_db.list_tables('CUSTOM-REF-1')
-print(tb_list)
-
-# ====================================
-# ☑️ THERMODB SETUP
-# ====================================
-# NOTE: set table
-table_ = 'NRTL Non-randomness parameters-1'
-
-# NOTE: get reference
-# property
-reference_config = {
-    'nrtl': {
-        'databook': databook_,
-        'table': table_,
-        'symbols': {
-            'alpha': 'alpha',
-            'a_i_j': 'a_i_j',
-            'b_i_j': 'b_i_j',
-            'c_i_j': 'c_i_j',
-        }
-    }
-}
-
-# ! yaml
-reference_config_yaml = """
-methanol|ethanol:
-    nrtl:
-        databook: CUSTOM-REF-1
-        table: NRTL Non-randomness parameters-1
-        symbols:
-            alpha: alpha
-            a_i_j: a_i_j
-            b_i_j: b_i_j
-            c_i_j: c_i_j
-"""
-
-# SECTION: define components
 # methanol
 methanol = Component(
     name='methanol',
@@ -244,7 +187,7 @@ methanol = Component(
 ethanol = Component(
     name='ethanol',
     formula='C2H5OH',
-    state='g'
+    state='l'
 )
 
 # methane
@@ -258,199 +201,64 @@ methane = Component(
 comp1 = methanol.name  # 'methanol'
 comp2 = ethanol.name  # 'ethanol'
 # >> multi-component
-components = [comp1, comp2]
+components = [methanol, ethanol]
 
-# ====================================
-# ☑️ GET MIXTURE DATA
-# ====================================
-# NOTE: get mixture data
-# ! METHANOL-ETHANOL MIXTURE
-# mixture_data = thermo_db.get_binary_mixture_data(
-#     components=[methanol, ethanol],
-#     databook=databook_,
-#     table=table_,
-#     component_key='Name-State',
-#     delimiter='|',
-#     ignore_component_state=True,
-# )
-# print(f"Mixture data: {mixture_data}")
+# >> mixture name
+mixture_name = f"{comp1}-{comp2}"
+print(f"mixture_name: {mixture_name}")
 
-# ! ETHANOL-METHANOL MIXTURE
-# mixture_data = thermo_db.get_binary_mixture_data(
-#     components=[ethanol, methanol],
-#     databook=databook_,
-#     table=table_,
-#     component_key='Name-State',
-#     delimiter='|',
-#     ignore_component_state=True,
-# )
-# print(f"Mixture data: {mixture_data}")
-
-# ====================================
-# ☑️ BUILD COMPONENTS THERMODB
-# ====================================
-# SECTION: build components thermodb
-# NOTE: build multi-component thermodb
-# ! set component key to 'Name' or 'Formula'
-thermodb_components_ = build_components_thermodb(
-    component_names=[comp1, comp2],
-    reference_config=reference_config,
-    custom_reference=custom_reference
+# SECTION: build component thermodb
+thermodb_components_: MixtureThermoDB = build_components_thermodb_from_reference(
+    components=components,
+    reference_content=REFERENCE_CONTENT,
 )
+print(f"thermodb_components_: {thermodb_components_}")
+
+# NOTE: thermodb
+thermodb_ = thermodb_components_.thermodb
+print(f"thermodb_: {thermodb_}")
 # check
-print(thermodb_components_.check())
-print(thermodb_components_.message)
+print(f"thermodb checks: {thermodb_.check()}")
 
-# ! reference config as yaml
-thermodb_components_yaml = build_components_thermodb(
-    component_names=[comp1, comp2],
-    reference_config=reference_config_yaml,
-    custom_reference=custom_reference
+# NOTE: build and save
+thermodb_component_save_: MixtureThermoDB = build_components_thermodb_from_reference(
+    components=components,
+    reference_content=REFERENCE_CONTENT,
+    thermodb_save=True,
+    thermodb_save_path=parent_path,
 )
+print(f"thermodb_component_save_: {thermodb_component_save_}")
 # check
-print(thermodb_components_yaml.check())
-print(thermodb_components_yaml.message)
+print(
+    f"thermodb_component_save_ checks: {thermodb_component_save_.thermodb.check()}")
 
-# SECTION: build multi-component thermodb
-# NOTE: check and build component thermodb
-# ! set component key to 'Name' or 'Formula' and 'State'
-thermodb_component_ = check_and_build_components_thermodb(
-    components=[methanol, ethanol],
-    reference_config=reference_config,
-    custom_reference=custom_reference,
-    component_key='Name-State',
-    mixture_key='Name',
-    ignore_state_all_props=True
+# SECTION: build component thermodb with ignore state
+# NOTE: ignore state for specific properties
+ignore_state_props = ['a']
+thermodb_component_ignore_state_: MixtureThermoDB = build_components_thermodb_from_reference(
+    components=components,
+    reference_content=REFERENCE_CONTENT,
+    ignore_state_props=ignore_state_props,
 )
+
+print(f"thermodb_component_ignore_state_: {thermodb_component_ignore_state_}")
+# NOTE: thermodb
+thermodb_ignore_state_ = thermodb_component_ignore_state_.thermodb
+print(f"thermodb_ignore_state_: {thermodb_ignore_state_}")
 # check
-print(thermodb_component_.check())
-print(thermodb_component_.message)
+print(f"thermodb_ignore_state_ checks: {thermodb_ignore_state_.check()}")
 
-# ! ignore state disabled
-thermodb_component_2 = check_and_build_components_thermodb(
-    components=[methanol, ethanol],
-    reference_config=reference_config,
-    custom_reference=custom_reference,
-    component_key='Name-State',
-    mixture_key='Name',
-    ignore_state_all_props=False
+# NOTE: build and save
+thermodb_component_ignore_state_save_: MixtureThermoDB = build_components_thermodb_from_reference(
+    components=components,
+    reference_content=REFERENCE_CONTENT,
+    ignore_state_props=ignore_state_props,
+    thermodb_save=True,
+    thermodb_save_path=parent_path,
+    thermodb_name=f"{mixture_name} with ignore state props",
 )
+print(
+    f"thermodb_component_ignore_state_save_: {thermodb_component_ignore_state_save_}")
 # check
-print(thermodb_component_2.check())
-print(thermodb_component_2.message)
-
-# ====================================
-# BUILD MATRIX DATA
-# ====================================
-# NOTE: build a matrix data
-nrtl_alpha = thermodb_components_.select('nrtl')
-# check
-if not isinstance(nrtl_alpha, TableMatrixData):
-    raise TypeError("Not TableMatrixData")
-
-# matrix table
-print(nrtl_alpha.matrix_table)
-# matrix table
-res_ = nrtl_alpha.get_matrix_table(mode='selected')
-print(res_, type(res_))
-
-# symbol
-print(nrtl_alpha.matrix_symbol)
-
-print(nrtl_alpha.matrix_data_structure())
-
-# matrix data
-print(nrtl_alpha.get_matrix_property(
-    "a_i_j",
-    [comp1, comp2],
-    symbol_format='alphabetic',
-    message="NRTL Alpha value")
-)
-
-print(nrtl_alpha.get_matrix_property(
-    "b_i_j",
-    [comp1, comp2],
-    symbol_format='alphabetic',
-    message="NRTL Alpha value")
-)
-
-# property name using ij method
-prop_name = f"a_{comp1}_{comp2}"
-print(prop_name)
-res_1 = nrtl_alpha.ij(prop_name)
-print(res_1)
-print(res_1.get('value'))
-
-# get property value using the matrix data
-# format 1
-# prop_name = f"dg_{comp1}_{comp2}"
-# format 2
-prop_name = f"a | {comp1} | {comp2}"
-# get values
-prop_matrix = nrtl_alpha.ijs(prop_name, res_format='alphabetic')
-print(prop_matrix, type(prop_matrix))
-
-print("*" * 20)
-prop_name = f"b | {comp1} | {comp2}"
-# get values
-prop_matrix = nrtl_alpha.ijs(prop_name, res_format='alphabetic')
-print(prop_matrix, type(prop_matrix))
-
-print("*" * 20)
-prop_name = f"c | {comp1} | {comp2}"
-# get values
-prop_matrix = nrtl_alpha.ijs(prop_name, res_format='alphabetic')
-print(prop_matrix, type(prop_matrix))
-prop_matrix = nrtl_alpha.ijs(prop_name, res_format='numeric')
-print(prop_matrix, type(prop_matrix))
-mat_ = nrtl_alpha.mat('c', [comp1, comp2])
-print(mat_)
-# get values
-prop_name = f"c | {comp2} | {comp1}"
-prop_matrix = nrtl_alpha.ijs(prop_name, res_format='alphabetic')
-print(prop_matrix, type(prop_matrix))
-prop_matrix = nrtl_alpha.ijs(prop_name, res_format='numeric')
-print(prop_matrix, type(prop_matrix))
-# ! ij matrix
-mat_ = nrtl_alpha.mat('c', [comp2, comp1])
-print(mat_)
-print("*" * 20)
-
-prop_name = f"alpha | {comp1} | {comp2}"
-# get values
-prop_matrix = nrtl_alpha.ijs(prop_name, res_format='alphabetic')
-print(prop_matrix, type(prop_matrix))
-
-# ! ij matrix
-# alpha
-mat_ = nrtl_alpha.mat('alpha', [comp2, comp1])
-print(mat_)
-print("*" * 20)
-
-# a matrix
-mat_ = nrtl_alpha.mat('a_i_j', [comp2, comp1])
-print(mat_)
-print("*" * 20)
-
-# a matrix
-mat_ = nrtl_alpha.mat('a_i_j', [comp1, comp2])
-print(mat_)
-
-# ====================================
-# SAVE THERMODB
-# ====================================
-# thermodb_file = thermodb_component_.thermodb_name or 'thermodb_component'
-
-# # save (pkl format)
-# res_ = thermodb_component_.save(thermodb_file, file_path=parent_dir)
-# print(f"ThermoDB saved: {res_}")
-
-# multi-component
-thermodb_file = thermodb_components_.thermodb_name or 'thermodb_component'
-
-# save (pkl format)
-res_ = thermodb_components_.save(
-    filename=thermodb_file,
-    file_path=parent_dir
-)
-print(f"ThermoDB saved: {res_}")
+print(
+    f"thermodb_component_ignore_state_save_ checks: {thermodb_component_ignore_state_save_.thermodb.check()}")
