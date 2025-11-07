6c87b6b9ad1f96ef307f4c9e907382927df6a047
sina.gilassi <sina.gilassi@gmail.com>
2025-11-07 01:33:07 -0500

    add key names and type

--- pyThermoDB/manager/main.py
+++ pyThermoDB/manager/main.py
@@ -1,11 +1,16 @@
 # import libs
-from typing import Dict
+from typing import Dict, Literal, List, Optional
 # locals
 from ..utils import EquationParser
 
 
 def parse_equation_body(
-    equation_body: str | Dict
+    equation_body: str | Dict,
+    key_mode: Literal[
+        'upper',
+        'lower'
+    ] = 'lower',
+    key_names: Optional[List[str]] = None
 ) -> list:
     '''
     Parse equation body string into structured dictionary.
@@ -14,6 +19,10 @@ def parse_equation_body(
     ----------
     equation_body : str or Dict
         The equation body string to parse.
+    key_mode : str, optional
+        Key mode (upper or lower), by default 'lower'
+    key_names : List[str], optional
+        Custom key names for the parsed dictionary, by default None. It use default key names as ['id', 'body', 'args', 'parms', 'return'].
 
     Returns
     -------
@@ -22,7 +31,12 @@ def parse_equation_body(
     '''
     try:
         # create parser instance
-        parser = EquationParser(equation_body)
+        parser = EquationParser(
+            equation_body=equation_body,
+            key_mode=key_mode,
+            key_names=key_names
+        )
+
         # parse equation
         parse_res = parser.eq_formatter()
 
