91a9c6aad93b8313483fa1a6cc7762b0c7428530
sina.gilassi <sina.gilassi@gmail.com>
2025-12-08 14:30:49 -0500

    Add unit extraction method to ReferenceChecker and enhance equation details retrieval

--- pyThermoDB/manager/main.py
+++ pyThermoDB/manager/main.py
@@ -1,7 +1,7 @@
 # import libs
 from typing import Dict, Any, List, Optional
 # locals
-from ..utils import EquationParser
+from ..utils import EquationParser, is_number
 from ..models import EquationDefinition
 
 
@@ -82,3 +82,100 @@ def parse_equation_body(
         return parser.eq_formatter()
     except Exception as e:
         raise Exception(f"parse_equation_body error! {e}")
+
+
+def parse_equation_body_with_table_structure(
+    equation_body: str | Dict[str, Any],
+    table_structure: Dict[str, List[Any]],
+    key_names: Optional[List[str]] = None
+) -> List[EquationDefinition]:
+    '''
+    Parse equation body string into structured dictionary (EquationDefinition) with respect to table structure for parms unit normalization.
+
+    Parameters
+    ----------
+    equation_body : str
+        The equation body string to normalize.
+    table_structure : Dict[str, List[Any]]
+        The table structure containing columns, symbol, unit information.
+    key_names : List[str], optional
+        Custom key names for the parsed dictionary, by default None. It use default key names as ['id', 'body', 'args', 'parms', 'return'].
+
+    Returns
+    -------
+    str
+        The normalized equation body string.
+    '''
+    try:
+        def BODY_ELEMENT_MAKER(
+                eq_parms: List[str],
+                symbols: List[Any],
+                units: List[Any]
+        ) -> List[str]:
+            # body config elements
+            BODY_ELEMENTS = []
+
+            # iterate over parms and unit
+            for symbol, unit in zip(symbols, units):
+                # check unit numeric
+                if (
+                    is_number(unit) and
+                    symbol in eq_parms
+                ):
+                    # construct full key
+                    element_ = f"parms['{symbol}'] = parms['{symbol}']/{unit}"
+
+                    # >> add to body config elements
+                    BODY_ELEMENTS.append(element_)
+
+            # return body elements
+            return BODY_ELEMENTS
+
+        # SECTION: create parser instance
+        parser = EquationParser(
+            equation_body=equation_body,
+            key_names=key_names
+        )
+
+        # parse equation
+        eqs_ = parser.eq_formatter()
+
+        # SECTION: parms unit normalization
+        SYMBOL = table_structure.get('SYMBOL', [])
+        UNIT = table_structure.get('UNIT', [])
+
+        # SECTION: normalize equation body
+        normalized_eqs: List[EquationDefinition] = []
+
+        # iterate equations
+        for eq in eqs_:
+            # get body
+            body_ = eq.body.copy()
+            # get parms
+            parms_ = eq.parms
+            # >> check
+            if parms_:
+                eq_parms = list(parms_.keys())
+            else:
+                eq_parms = []
+
+            # make body elements
+            BODY_ELEMENTS = BODY_ELEMENT_MAKER(
+                eq_parms=eq_parms,
+                symbols=SYMBOL,
+                units=UNIT
+            )
+
+            # prepend body elements
+            body_ = BODY_ELEMENTS + body_
+
+            # update body
+            eq.body = body_
+
+            # >> add to normalized equations
+            normalized_eqs.append(eq)
+
+        # return normalized equations
+        return normalized_eqs
+    except Exception as e:
+        raise Exception(f"normalize_equation_body error! {e}")
