9035b95b503aa339a57dada1986be15ad304cfa9
sina.gilassi <sina.gilassi@gmail.com>
2025-12-08 14:30:40 -0500

    upd

--- pyThermoDB/manager/__init__.py
+++ pyThermoDB/manager/__init__.py
@@ -1,8 +1,12 @@
 # export
 from .managedata import ManageData
-from .main import parse_equation_body
+from .main import (
+    parse_equation_body,
+    parse_equation_body_with_table_structure
+)
 
 __all__ = [
     'ManageData',
-    'parse_equation_body'
+    'parse_equation_body',
+    'parse_equation_body_with_table_structure'
 ]
