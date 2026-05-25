9ecf2d56b950fa29c70c7c75db26c08cdf47d373
sina.gilassi <sina.gilassi@gmail.com>
2026-05-25 14:17:10 -0400

    feat: add TableConstant class with logging setup and type annotations

---
+++ pyThermoDB/core/tableconstant.py
@@ -0,0 +1,12 @@
+# import libs
+import logging
+from typing import Any, Dict, List, Optional, Union
+# locals
+
+# NOTE: logger setup
+logger = logging.getLogger(__name__)
+
+
+class TableConstant:
+    def __init__(self):
+        pass
