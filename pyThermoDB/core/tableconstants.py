d481ddbb0fe2b51b98773d216003bf64a467cbbc
sina.gilassi <sina.gilassi@gmail.com>
2026-05-26 19:04:25 -0400

    feat: implement __init__ method in TableConstants class with parameters for databook and table details

--- pyThermoDB/core/tableconstants.py
+++ pyThermoDB/core/tableconstants.py
@@ -8,5 +8,17 @@ logger = logging.getLogger(__name__)
 
 
 class TableConstants:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        databook_name,
+        table_name,
+        table_data,
+        table_values: Optional[List | Dict] = None,
+        table_structure: Optional[Dict[str, Any]] = None
+    ):
+        # NOTE: set
+        self.databook_name = databook_name
+        self.table_name = table_name
+        self.table_data = table_data
+        self.table_values = table_values
+        self.table_structure = table_structure
