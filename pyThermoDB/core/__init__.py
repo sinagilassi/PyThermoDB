93f9d4de9af46b40ee2fc7ad14f6727301ca8aa3
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-09-20 23:37:37 -0400

    feat: include TableInteractionData in module exports

--- pyThermoDB/core/__init__.py
+++ pyThermoDB/core/__init__.py
@@ -2,6 +2,7 @@
 from .tabledata import TableData
 from .tableequation import TableEquation
 from .tablematrixdata import TableMatrixData
+from .tableinteractiondata import TableInteractionData
 from .tablematrixequation import TableMatrixEquation
 from .tableconstants import TableConstants
 from .table_util import TableUtil
@@ -10,6 +11,7 @@ __all__ = [
     'TableData',
     'TableEquation',
     'TableMatrixData',
+    'TableInteractionData',
     'TableMatrixEquation',
     'TableConstants',
     'TableUtil',
