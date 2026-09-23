21b2f7a6400ea26814a4febcc451a3d2a7157cf9
sina.gilassi <sina.gilassi@gmail.com>
2026-09-23 01:27:43 -0400

    docs: enhance docstring for TableTypes enum with detailed attribute descriptions

--- pyThermoDB/data/ref.py
+++ pyThermoDB/data/ref.py
@@ -3,10 +3,30 @@ from enum import Enum
 
 
 class TableTypes(Enum):
-    """Enum class for table types"""
+    """
+    Enum class for table types
+
+    Attributes
+    ----------
+    EQUATIONS : str
+        Table type for equations
+    DATA : str
+        Table type for data
+    MATRIX_EQUATIONS : str
+        Table type for matrix equations
+    MATRIX_DATA : str
+        Table type for matrix data
+    INTERACTION_DATA : str
+        Table type for interaction data
+    CONSTANTS : str
+        Table type for constants
+    DATASET : str
+        Table type for dataset
+    """
     EQUATIONS = "equations"
     DATA = "data"
     MATRIX_EQUATIONS = "matrix_equations"
     MATRIX_DATA = "matrix_data"
     INTERACTION_DATA = "interaction_data"
     CONSTANTS = "constants"
+    DATASET = "dataset"
